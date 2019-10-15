2014dde9aa549859a871bf3782af17f71cceea6a
pftom <1043269994@qq.com>
2019-10-15 11:05:19 +0800

    允许资源跨域访问

--- app.js
+++ app.js
@@ -15,8 +15,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Datbase connection here
-
 mongoose.connect(`mongodb://localhost:27017/test`);
+
+// CORS config here
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
 
 
 // uncomment after placing your favicon in /public
