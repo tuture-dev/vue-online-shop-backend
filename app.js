e5edda0ed11bfab1b0350b2dc70f9cef80b5563c
pftom <1043269994@qq.com>
2019-10-15 11:02:03 +0800

    接入 MongoDB 数据库

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +13,11 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Datbase connection here
+
+mongoose.connect(`mongodb://localhost:27017/test`);
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
